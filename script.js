2577be001f1c28cbd8bd1222d96618ebc9e47114
Andres <villegasa25@gmail.com>
2021-10-20 21:08:07 +0000

    changes made in the js

--- script.js
+++ script.js
@@ -14,6 +14,20 @@ var interest = principal * years * rate /100;
  var year = new Date().getFullYear()+parseInt(years);
     function compute() //sets the inner html of result
 {
+    var principal = document.getElementById("principal").value;
+if(principal<=0){ //Checks and sends an alert of the value of principal being less or equal to 0
+   $.alert({  //then creates an alert an once it hits ok it focueses on the principal input field
+      title: 'Alert!',
+      content: 'Enter a positive number',
+      onDestroy: function() {
+        // here the button key 'hey' will be used as the text.
+        $('#Principal').focus();
+      }
+})};
+var rate = document.getElementById("rate").value //brings all the values from the html
+var years = document.getElementById("years").value;
+var interest = principal * years * rate /100;
+ var year = new Date().getFullYear()+parseInt(years);
     p = document.getElementById("principal").value;
     var res= document.getElementById("result");
     res.innerHTML= "If you deposit <mark>"+ principal+ "</mark><br> at an interest rate of <mark>" + rate + "</mark>.<br> You will receive an amount of <mark>" + interest+ "</mark>,<br> in the year <mark>"+year+"</mark>.";
